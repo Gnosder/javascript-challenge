4e00b2e9827f26e18ef912776675e6eacc3d3425
Patrick Plaisted <patrick.plaisted@gmail.com>
2020-09-12 20:51:44 -0700

    updated level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,13 +2,34 @@
 var tableData = data;
 
 // Reference to table body
-var tbody = d3.select("#ufo-table");
+var tbody = d3.select("tbody");
 
-// 
-data.forEach((tableData) => {
-    var row = tbody.append("tr");
-    Object.entries(tableData).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-    })
-});
+
+// Append table for each sighting
+function popTable(d) {
+    d.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        })
+    });
+}
+
+
+// Filter by date form
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+button.on("click", filterByDate);
+form.on("submit", filterByDate);
+function filterByDate() {
+    d3.event.preventDefault();
+    tbody.html("")
+    var inputEle = d3.select(".form-control");
+    var inputVal = inputEle.property("value");
+    var filteredData = tableData.filter(item => item.datetime === inputVal);
+    popTable(filteredData);
+}
+
+
+popTable(tableData);
